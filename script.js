6684b6cd1eb5bfe4fbcaf967874a1d00065c50de
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-12-24 16:34:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function CLIQ() {
   
  document.getElementById('demo').innerHTML='Christmas is a season of enjoying the simple things that make life beautiful. May you have great memories that will permanently touch your heart. I wish you so much joy during this season and all through to the New Year.'; 
  
- document.getElementById('Image').src = ""; 
+ document.getElementById('Image').src = "Christmas_gif.gif"; 
   
 }
